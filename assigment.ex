ecfbd473ed05cf39edec18473377e02f4b952dba
Mohanned Mohammed <45148948+MohannedM@users.noreply.github.com>
2021-04-01 10:30:05 +0200

    refactor fix

--- assigment.ex
+++ assigment.ex
@@ -3,9 +3,9 @@ defmodule User do
   def initials(name) do
     with split_name <- String.split(name, " "),
     true <- split_name |> length >= 2 do
-      [first_name | last_names ] = split_name
-      [last_name | _] = Enum.reverse(last_names)
-      "#{String.slice(first_name, 0, 1)}#{String.slice(last_name, 0, 1)}"
+        first_letter = split_name |> List.first() |> String.slice(0, 1)
+        last_letter = split_name |> List.last() |> String.slice(0, 1)
+      "#{first_letter}#{last_letter}"
     else
       false -> name |> String.slice(0, 1)
     end
